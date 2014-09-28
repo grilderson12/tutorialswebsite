8110a6cfceabcbc30e8b6ede3de2303d1d481b5d
grilderson12 <grilderson12@gmail.com>
2014-09-28 21:08:05 +0800

    Update signup-msg.php

--- signup-msg.php
+++ signup-msg.php
@@ -1,5 +1,48 @@
 <?php
 
+require "dbconfig.php";
 
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+  if($email) {
+    
+  echo "Sign UP now :)";
+    
+    if($password) {
+      
+      echo $password, " is accept it.";
+      
+    }else {
+      
+      echo "Wrong Password";
+      
+    }
+      
+      if($username) {
+        
+        echo $username, " is accept it";
+        
+      }else {
+        
+        echo "Wrong Username";
+        
+      }
+      
+        if($username&&$password) {
+          
+          echo 'Congrats your has been accept it.you can login now.';
+          
+          $insert = 'INSERT into users(username, password, email) VALUES("'.$username.'","'.$password.'","'.$email.'")';
+          mysql_query($insert);
+          
+        }else {
+          
+          echo 'Fail to LOGIN, Please try again now.';
+          
+        }
+    
+  }
 
 ?>
