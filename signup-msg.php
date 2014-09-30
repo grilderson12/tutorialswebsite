844698516c332f4862d6be14b665fc16df7fe141
grilderson12 <grilderson12@gmail.com>
2014-09-30 11:05:34 +0800

    Update signup-msg.php

--- signup-msg.php
+++ signup-msg.php
@@ -1,44 +1,46 @@
 <?php
 
-	echo "SIGN UP NOW<br />";
-
-require "dbconnect.php";
+require 'dbconnect.php';
 
 $username = $_POST["username"];
-$password = $_post["password"];
+$password = $_POST["password"];
 $email = $_POST["email"];
+$signup = $_POST["signup"];
 
-if($email) {
-
-  if($password) {
-    
-    echo $password, " is accept it.<br />";
-    
-    if($username) {
-      
-      echo $username, "is accept it.";
-      
-        if($username&&$password) {
-          
-          echo "Congrats your account has been take it.";
-          
-          $insert 'INSERT into users(username, password, email) VALUES("'.$username.'","'.$password.'","'.$email.'")';
-          mysql_query($insert);
-          
-        }
-      
-    }else {
-    
-      echo "Wrong username.";  
-      
-    }
-    
-  }else {
-    
-    echo "Wrong password.";
-    
-  }
-
+if($signup) {
+	
+	if($password&&$email) {
+		echo $password, " is accept it.<br />";
+		
+			if($username) {
+				
+				echo $username, " is accept it.<br />";
+				
+					if($username&&$password) {
+						
+						echo "Congrats your account have been take it.<br />";
+						
+						$insert = 'INSERT into users(username, password, email) VALUES("'.$username.'","'.$password.'","'.$email.'")';
+						mysql_query($insert);
+						
+					}else {
+						
+						echo "Fail to Register, Please try again now<br />";
+						
+					}
+				
+			}else {
+				
+				echo "Wrong username<br />";
+				
+			}
+		
+	}else {
+		echo "Wrong Password<br />";
+		
+		
+	}
+	
 }
 
 ?>
